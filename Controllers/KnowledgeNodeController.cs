79aa9724df3e814e1b1e33b48d5029ae487ae8ef
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-07 14:41:43 -0600

    starting on controller for KNs

---
+++ Controllers/KnowledgeNodeController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Knowledge_Center_API.Models;
+using Knowledge_Center_API.Services.Core;
+
+namespace Knowledge_Center_API.Controllers
+{
+    public class KnowledgeNodeController : ControllerBase
+    {
+    }
+}
