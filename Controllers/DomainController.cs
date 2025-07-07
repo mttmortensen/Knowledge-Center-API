52aa2091507f6a59d7362065abff20546f1711bd
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-07 15:13:05 -0600

    now starting on domain controller. constructor setup

---
+++ Controllers/DomainController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Knowledge_Center_API.Models;
+using Knowledge_Center_API.Services.Core;
+
+namespace Knowledge_Center_API.Controllers
+{
+
+    [ApiController]
+    [Route("api/domains")]
+    public class DomainController : ControllerBase
+    {
+        private readonly DomainService _domainService;
+
+        public DomainController(DomainService domainService)
+        {
+            _domainService = domainService;
+        }
+    }
+}
