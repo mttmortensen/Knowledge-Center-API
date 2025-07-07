6a3811d6538caa8280dc0b84dd89bd46139d1509
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-07 14:47:46 -0600

    setting up constructor

--- Controllers/KnowledgeNodeController.cs
+++ Controllers/KnowledgeNodeController.cs
@@ -4,7 +4,17 @@ using Knowledge_Center_API.Services.Core;
 
 namespace Knowledge_Center_API.Controllers
 {
+    [ApiController]
+    [Route("/api/knowledge-nodes")]
     public class KnowledgeNodeController : ControllerBase
     {
+        private readonly KnowledgeNodeService _knowledgeNodeService;
+        private readonly LogEntryService _logEntryService;
+
+        public KnowledgeNodeController(KnowledgeNodeService knService, LogEntryService lgService) 
+        {
+            _knowledgeNodeService = knService;
+            _logEntryService = lgService;
+        }
     }
 }
