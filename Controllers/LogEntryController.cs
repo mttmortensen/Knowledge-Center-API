f58f30579abbd46fa9a23caf990ba430ad737a00
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-07 15:04:31 -0600

    now moving onto the log entry domain

---
+++ Controllers/LogEntryController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Knowledge_Center_API.Models;
+using Knowledge_Center_API.Services.Core;
+
+namespace Knowledge_Center_API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LogEntriesController : ControllerBase
+    {
+        private readonly LogEntryService _logEntryService;
+
+        public LogEntriesController(LogEntryService logEntryService)
+        {
+            _logEntryService = logEntryService;
+        }
+    }
+}
