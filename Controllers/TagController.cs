8dda2f1537040db8152d31d49f2ad93c23c60122
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-07 15:24:09 -0600

    doing the final domain, tags and the controller

---
+++ Controllers/TagController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Knowledge_Center_API.Models;
+using Knowledge_Center_API.Services.Core;
+
+namespace Knowledge_Center_API.Controllers
+{
+    [ApiController]
+    [Route("api/tags")]
+    public class TagController : ControllerBase
+    {
+        private readonly TagService _tagService;
+
+        public TagController(TagService tagService)
+        {
+            _tagService = tagService;
+        }
+    }
+}
