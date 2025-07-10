ca42bd81b6af5fb9727e56c5d28725fed6bf7270
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-10 14:59:45 -0600

    I need to do a log dto during post so that I won't need to bring in the Tag value

---
+++ Models/DTOs/LogEntryCreateDto.cs
@@ -0,0 +1,17 @@
+﻿namespace Knowledge_Center_API.Models.DTOs
+{
+    /*
+     * POST /api/logs
+     * 
+     * This DTO is going to be used in only creating 
+     * of a Log Entry as we don't need to pull in the 
+     * Tag name/value as the front end will handle this
+     */
+    public class LogEntryCreateDto
+    {
+        public int NodeId { get; set; }
+        public int TagId { get; set; }
+        public string Content { get; set; }
+        public bool ContributesToProgress { get; set; }
+    }
+}
