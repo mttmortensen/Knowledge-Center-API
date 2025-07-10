0bd3927ef80f5892e094ba95b51668bc3978efe4
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-10 09:33:13 -0600

    I need to lock down the auth to be on the back for these routes as well

---
+++ Services/Security/RequireTokenAttribute.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Knowledge_Center_API.Services.Security
+{
+    public class RequireTokenAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            
+        }
+    }
+}
