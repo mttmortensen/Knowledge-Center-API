540570f0bf0557b7194af7e625e0924e32b2b4b4
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-10 08:56:09 -0600

    login logic using the LoadTstCreds from AuthHelper

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,4 +1,5 @@
 ﻿using Knowledge_Center_API.Models;
+using Knowledge_Center_API.Services.Security;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Knowledge_Center_API.Controllers
@@ -10,7 +11,32 @@ namespace Knowledge_Center_API.Controllers
         [HttpPost("login")]
         public IActionResult Login([FromBody] LoginRequest loginData) 
         {
-            return Ok();
+            try
+            {
+                // === Step 1: Validate (sortof) login data
+                if (loginData == null || string.IsNullOrWhiteSpace(loginData.Username) || string.IsNullOrWhiteSpace(loginData.Password))
+                {
+                    return BadRequest(new { message = "Missing Creds" });
+                }
+
+                // === Step 2: Authenticate
+                // This avoids hard coded creds
+                var (expectedUsername, expectedPassword) = AuthHelper.LoadTestCredentials();
+
+                if (loginData.Username == expectedUsername && loginData.Password == expectedPassword) 
+                {
+                    string token = AuthSession.CreateSession(expectedUsername);
+                    return Ok(new { token });
+                }
+                else 
+                {
+                    return BadRequest(new { message = "Invalid Creds" });
+                }
+            }
+            catch 
+            {
+                return StatusCode(500, new { messsage = "Login Request Failed." });
+            }
         }
 
         [HttpPost("logout")]
