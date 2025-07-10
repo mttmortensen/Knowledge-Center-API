2247070eb81f160e0e1b09aef50a8ab826ff899c
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-10 08:39:30 -0600

    now working on the converstion to bring over auth from raw kc c#. logic next

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,22 @@
+﻿using Knowledge_Center_API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Knowledge_Center_API.Controllers
+{
+    [ApiController]
+    [Route("api/auth")]
+    public class AuthController : Controller
+    {
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginRequest loginData) 
+        {
+            return Ok();
+        }
+
+        [HttpPost("logout")]
+        public IActionResult Logout() 
+        {
+            return Ok();
+        }
+    }
+}
