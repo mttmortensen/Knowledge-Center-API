72945e7790c89eb6e7fdf7d0a352f3f12ed39498
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-11 13:25:57 -0600

    creating a user model to help with cleaner service logic for userservice

---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace Knowledge_Center_API.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+        
+        // Auth flow helpers
+        public bool IsAuthenticated { get; set; } = false;
+        public string Token { get; set; } = null;
+    }
+}
