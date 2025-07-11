3c5b0907849e5f0d0932acacf5113df4610139b0
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-10 22:01:43 -0600

    Needed an update dto for domains now

---
+++ Models/DTOs/DomainUpdateDto.cs
@@ -0,0 +1,17 @@
+﻿namespace Knowledge_Center_API.Models.DTOs
+{
+    /*
+     * PUT /api/domains
+     * 
+     * This DTO is going to be used in only updating
+     * A Domain as we don't need update all the 
+     * name/values.
+     */
+    public class DomainUpdateDto
+    {
+        public int DomainId { get; set; }
+        public string? DomainName { get; set; }
+        public string? DomainDescription { get; set; }
+        public string? DomainStatus { get; set; }
+    }
+}
