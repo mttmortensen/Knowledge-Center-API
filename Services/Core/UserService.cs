03a9af25c346395a5bbf617c9af1dd5b960483f8
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-11 14:44:58 -0600

    adding a user service now. This will really only authenticate the user

---
+++ Services/Core/UserService.cs
@@ -0,0 +1,22 @@
+﻿using Knowledge_Center_API.Models;
+
+namespace Knowledge_Center_API.Services.Core
+{
+    public class UserService
+    {
+        
+        
+        
+        /* ===================== DATA TYPE CONVERTERS (MAPPERS) ===================== */
+        private User ConvertDBRowToUser(Dictionary<string, object> row)
+        {
+            return new User
+            {
+                Id = Convert.ToInt32(row["Id"]),
+                Username = row["Username"].ToString(),
+                PasswordHash = row["PasswordHash"].ToString()
+                // IsAuthenticated and Token are set during login
+            };
+        }
+    }
+}
