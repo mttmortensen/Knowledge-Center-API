15bc831cdfbf5b33c6f61a8e112899884a3ce57c
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-11 19:05:27 -0600

    logic for authenticating the user

--- Services/Core/UserService.cs
+++ Services/Core/UserService.cs
@@ -1,10 +1,52 @@
-﻿using Knowledge_Center_API.Models;
+﻿using Knowledge_Center_API.DataAccess;
+using Knowledge_Center_API.Models;
+using Knowledge_Center_API.Services.Security;
+using Knowledge_Center_API.Services.Validation;
+using Microsoft.Data.SqlClient;
 
 namespace Knowledge_Center_API.Services.Core
 {
     public class UserService
     {
-        
+        private readonly Database _db;
+
+        public UserService(Database db)
+        {
+            _db = db;
+        }
+
+        /* ==== Validate User ==== */
+        public User AuthenticateUser(string username, string password) 
+        {
+            // Validate Inputs 
+            FieldValidator.ValidateRequiredString(username, "Username", 100);
+            FieldValidator.ValidateRequiredString(password, "Password", 100);
+
+            // Query User from DB
+            List<SqlParameter> parameters = new List<SqlParameter> 
+            {
+                new SqlParameter("@Username", username)
+            };
+
+            var result = _db.ExecuteQuery(UserQueries.GerUserByUsername, parameters);
+
+            if (result.Count == 0)
+                return new User { IsAuthenticated = false };
+
+            var user = ConvertDBRowToUser(result[0]);
+
+            // Verifying password
+            if (!AuthHelper.VerifyPassword(password, user.PasswordHash)) 
+            {
+                user.IsAuthenticated = false;
+                return user;
+            }
+
+            // Generating Token
+            user.Token = AuthSession.CreateSession(user.Username);
+            user.IsAuthenticated = true;
+            return user;
+        }
         
         
         /* ===================== DATA TYPE CONVERTERS (MAPPERS) ===================== */
