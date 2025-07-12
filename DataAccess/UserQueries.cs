84f7cd9d274d16b6893e30a837007677c6beae9c
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-11 19:05:16 -0600

    query class for getting a username

---
+++ DataAccess/UserQueries.cs
@@ -0,0 +1,10 @@
+﻿namespace Knowledge_Center_API.DataAccess
+{
+    public static class UserQueries
+    {
+        public static readonly string GerUserByUsername = @"
+            SELECT * FROM Users
+            WHERE Username = @Username;
+        ";
+    }
+}
