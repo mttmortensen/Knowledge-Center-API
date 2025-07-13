3738bad36d1b22b866b20dde5abd7fa179c290f6
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-12 18:11:07 -0600

    I've added two new Dtos to help with adding Logs to a KN. This will help with get all and get single on front end or just the api in general

---
+++ Models/DTOs/KnowledgeNodeWithLogsDto.cs
@@ -0,0 +1,18 @@
+﻿namespace Knowledge_Center_API.Models.DTOs
+{
+    public class KnowledgeNodeWithLogsDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int DomainId { get; set; }
+        public string NodeType { get; set; } 
+        public string Description { get; set; }
+        public int ConfidenceLevel { get; set; }
+        public string Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastUpdated { get; set; }
+
+        public List<LogEntryInlineDto> Logs { get; set; }
+
+    }
+}
---
+++ Models/DTOs/LogEntryInlineDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Knowledge_Center_API.Models.DTOs
+{
+    public class LogEntryInlineDto
+    {
+        public int LogId { get; set; }
+        public string Content { get; set; }
+        public DateTime EntryDate { get; set; }
+        public bool ContributesToProgress { get; set; }
+    }
+}
