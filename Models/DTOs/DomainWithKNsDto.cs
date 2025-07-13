6992ed076a29cb65e6a6487e4e9a9d0850aa4b6e
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-13 00:46:15 -0600

    Now linking up KNs to a Domain. Using Dtos to get this done

---
+++ Models/DTOs/DomainWithKNsDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Knowledge_Center_API.Models.DTOs
+{
+    public class DomainWithKNsDto
+    {
+        public int DomainId { get; set; }
+        public string DomainName { get; set; }
+        public string DomainDescription { get; set; }
+        public string DomainStatus { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastUsed { get; set; }
+        public DateTime LastUpdated { get; set; }
+        public List<KnowledgeNodeInlineDto> KnowledgNodes { get; set; }
+    }
+}
---
+++ Models/DTOs/KnowledgeNodeInlineDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Knowledge_Center_API.Models.DTOs
+{
+    public class KnowledgeNodeInlineDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string NodeType { get; set; }
+        public int ConfidenceLevel { get; set; }
+        public string Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+}
