5f582b70ecc649db5a0b3fbcfba0469286003a48
mttmortensen <mtt.mortensen527@gmail.com>
2025-08-06 19:49:27 -0600

    Added dto for chatURL to be allowed updated for a logentry

---
+++ Models/DTOs/LogEntryUpdateDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Knowledge_Center_API.Models.DTOs
+{
+
+    /*
+     * PUT /api/logs
+     * 
+     * The only prop I'll only to be updated aside from Tags
+     * but that's its own route and servicing
+     * 
+     */
+    public class LogEntryUpdateDto
+    {
+        public string? ChatURL { get; set; }
+    }
+}
