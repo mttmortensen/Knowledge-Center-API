81300cc3e017aa5777ce75388de4612f4ea1eebf
mttmortensen <mtt.mortensen527@gmail.com>
2025-08-08 11:43:31 -0600

    needed to add new dtos just for listings/GET

---
+++ Models/KnowledgeNodes/KnowledgeNodeListDto.cs
@@ -0,0 +1,18 @@
+﻿using Knowledge_Center_API.Models.LogEntries;
+
+namespace Knowledge_Center_API.Models.KnowledgeNodes
+{
+    public class KnowledgeNodeListDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int DomainId { get; set; }
+        public string NodeType { get; set; }
+        public string Description { get; set; }
+        public int ConfidenceLevel { get; set; }
+        public string Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastUpdated { get; set; }
+        public List<LogEntryListDto> Logs { get; set; }
+    }
+}
---
+++ Models/LogEntries/LogEntryListDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Knowledge_Center_API.Models.LogEntries
+{
+    public class LogEntryListDto
+    {
+        public int Id { get; set; }
+        public DateTime EntryDate { get; set; }
+    }
+}
